039c83a4118f0c18fa1ab66512f5415de22c444d
aburifat <rifat.cse4.bu@gmail.com>
2022-04-21 06:23:57 +0600

    Init LightOj folder

---
+++ LightOJ/critical-links.cpp
@@ -0,0 +1,27 @@
+// /*******Bismillahir-Rahmanir-Rahim**********/
+// 
+// Name         : Abu Rifat Muhammed Al Hasib
+// Institution  : University of Barisal, Bangladesh
+// Github       : https://github.com/abu-rifat
+// website	: https://aburifat.com
+// 
+// Codeforces   : https://codeforces.com/profile/ARMaster
+// lightoj      : https://lightoj.com/user/abu-rifat
+// AtCoder      : https://atcoder.jp/users/aburifat
+// codechef     : https://www.codechef.com/users/aburifat
+// SPOJ         : https://www.spoj.com/users/abu_rifat
+// UvaOJ        : https://uhunt.onlinejudge.org/id/889274
+// 
+// facebook     : https://facebook.com/AbuRifatM
+// linkedin     : https://www.linkedin.com/in/aburifat
+// Email        : rifat.cse4.bu@gmail.com
+// 		  armalhasib@gmail.com
+// 		  abu.rifat.m@gmail.com
+// 
+// Problem	: Critical Links
+// Contest	: LightOJ
+// URL		: https://lightoj.com/problem/critical-links
+// Memory Limit : 64 MB
+// Time Limit	: 1000 ms
+
+
